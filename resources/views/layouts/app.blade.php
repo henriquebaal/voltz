9f494d143b7a8855229666b1887ede794f5c5177
henriquebaaljunior@hotmail.com <henriquebaaljunior@hotmail.com>
2024-10-29 17:54:54 -0300

    Ajusta fontes no layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-    
     <link rel="icon" href="{{ asset('simbolo.png') }}" type="image/png"/>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,91 +9,136 @@
     <!-- Incluir Bootstrap e Font Awesome para a navbar e ícones -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    
 
     <!-- Estilos personalizados -->
     <style>
-    .navbar {
-        margin-bottom: 0;
-    }
+        /* Configuração da Navbar */
+        .navbar {
+            margin-bottom: 0;
+        }
 
-    /* Botão do carrinho fixado no lado direito */
-    .cart-button {
-        position: fixed; /* Fixo no lado direito */
-        bottom: 100px;
-        right: 20px; /* Posição no lado direito */
-        z-index: 1000; /* Para garantir que fique sobre o conteúdo */
-        margin: 0;
-    }
+        /* Botão do carrinho fixado no lado direito */
+        .cart-button {
+            position: fixed;
+            bottom: 100px;
+            right: 20px;
+            z-index: 1000;
+            margin: 0;
+        }
 
-    /* Estilo básico para o submenu */
-    .dropdown-submenu {
-        position: relative;
-    }
+        /* Estilo básico para o submenu */
+        .dropdown-submenu {
+            position: relative;
+        }
 
-    /* Quando passar o mouse sobre o item de menu, mostrar o submenu à esquerda */
-    .dropdown-submenu .dropdown-menu {
-        display: none;
-        top: 0;
-        left: -200px; /* Ajustando o submenu para a esquerda */
-        margin-top: -6px;
-        border-radius: 0.25rem;
-    }
+        /* Quando passar o mouse sobre o item de menu, mostrar o submenu à esquerda */
+        .dropdown-submenu .dropdown-menu {
+            display: none;
+            top: 0;
+            left: -200px;
+            margin-top: -6px;
+            border-radius: 0.25rem;
+        }
 
-    /* Exibir o submenu quando o mouse passa sobre o item de menu */
-    .dropdown-submenu:hover .dropdown-menu {
-        display: block;
-    }
+        .dropdown-submenu:hover .dropdown-menu {
+            display: block;
+        }
 
+        /* Ajustando a largura do submenu */
+        .dropdown-menu {
+            width: 200px;
+        }
 
-    /* Ajustando a largura do submenu para melhorar a aparência */
-    .dropdown-menu {
-        width: 200px;
-    }
-    .star-rating {
-        display: flex;
-        flex-direction: row-reverse;
-        justify-content: center;
-    }
+        /* Estilos de avaliação com estrelas */
+        .star-rating {
+            display: flex;
+            flex-direction: row-reverse;
+            justify-content: center;
+        }
 
-    .star-rating input[type="radio"] {
-        display: none; /* Esconder os botões de rádio */
-    }
+        .star-rating input[type="radio"] {
+            display: none;
+        }
 
-    .star-rating label {
-        font-size: 2em;
-        color: #ddd;
-        cursor: pointer;
-    }
+        .star-rating label {
+            font-size: 2em;
+            color: #ddd;
+            cursor: pointer;
+        }
 
-    .star-rating input[type="radio"]:checked ~ label {
-        color: #ffc107; /* Cor das estrelas selecionadas */
-    }
+        .star-rating input[type="radio"]:checked ~ label,
+        .star-rating label:hover,
+        .star-rating label:hover ~ label {
+            color: #ffc107;
+        }
 
-    .star-rating label:hover,
-    .star-rating label:hover ~ label {
-        color: #ffc107; /* Cor das estrelas ao passar o mouse */
-}
-    .pagination .page-link {
-        font-size: 1rem; /* Define o tamanho padrão do texto */
-        padding: 0.5rem 0.75rem; /* Ajusta o padding dos links */
-}
+        /* Estilos de paginação */
+        .pagination .page-link {
+            font-size: 1rem;
+            padding: 0.5rem 0.75rem;
+        }
 
-    .page-item .page-link svg {
-        width: 1rem; /* Define um tamanho menor para os ícones */
-        height: 12rem;
-}
+        .page-item .page-link svg {
+            width: 1rem;
+            height: 1rem;
+        }
 
-    /* Outros estilos */
+        /* Adicionar espaçamento inferior ao conteúdo da página */
+        .container {
+            padding-bottom: 50px;
+        }
 
-    /* Adicionar espaçamento inferior ao conteúdo da página */
-    .container {
-        padding-bottom: 50px; /* Adiciona um espaçamento inferior */
-    }
+        /* Importando a fonte RedHatDisplay com variantes */
+        @font-face {
+            font-family: 'RedHatDisplay';
+            src: url('{{ asset('fonts/RedHatDisplay-Regular.ttf') }}') format('truetype');
+            font-weight: 400;
+            font-style: normal;
+        }
 
+        @font-face {
+            font-family: 'RedHatDisplay';
+            src: url('{{ asset('fonts/RedHatDisplay-Italic.ttf') }}') format('truetype');
+            font-weight: 400;
+            font-style: italic;
+        }
 
+        @font-face {
+            font-family: 'RedHatDisplay';
+            src: url('{{ asset('fonts/RedHatDisplay-Bold.ttf') }}') format('truetype');
+            font-weight: 700;
+            font-style: normal;
+        }
 
-</style>
+        @font-face {
+            font-family: 'RedHatDisplay';
+            src: url('{{ asset('fonts/RedHatDisplay-BoldItalic.ttf') }}') format('truetype');
+            font-weight: 700;
+            font-style: italic;
+        }
+
+        @font-face {
+            font-family: 'RedHatDisplay';
+            src: url('{{ asset('fonts/RedHatDisplay-Black.ttf') }}') format('truetype');
+            font-weight: 900;
+            font-style: normal;
+        }
+
+        @font-face {
+            font-family: 'RedHatDisplay';
+            src: url('{{ asset('fonts/RedHatDisplay-BlackItalic.ttf') }}') format('truetype');
+            font-weight: 900;
+            font-style: italic;
+        }
+
+        /* Adicionar outras variantes de fontes aqui conforme necessário */
+
+        /* Aplicar a fonte personalizada em todo o site */
+        body {
+            font-family: 'RedHatDisplay', sans-serif;
+        }
+
+    </style>
 
     @yield('styles')
 </head>
@@ -103,15 +147,14 @@
     @include('layouts.navbar')
 
     <!-- Exibir o botão de carrinho apenas na home -->
-@if(Route::currentRouteName() == 'home')
-    <a href="{{ route('cart.show') }}" class="btn btn-success cart-button">
-        <i class="fas fa-shopping-cart"></i> Carrinho 
-        <span class="badge badge-light" id="cart-count">
-            {{ session('cart') ? count(session('cart')) : 0 }}
-        </span>
-    </a>
-@endif
-
+    @if(Route::currentRouteName() == 'home')
+        <a href="{{ route('cart.show') }}" class="btn btn-success cart-button">
+            <i class="fas fa-shopping-cart"></i> Carrinho 
+            <span class="badge badge-light" id="cart-count">
+                {{ session('cart') ? count(session('cart')) : 0 }}
+            </span>
+        </a>
+    @endif
 
     <!-- Conteúdo Principal -->
     <div class="container mt-5">
